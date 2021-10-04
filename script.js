bb61898926bc8d19d0c122d240ade12c975e8679
OniHony <90738273+OniHony@users.noreply.github.com>
2021-10-03 23:59:35 -0400

    Java Push

--- script.js
+++ script.js
@@ -3,26 +3,41 @@ const choice = ["Rock", "Paper", "Scissors"];
 const computerPlay = function () {
   return choice[Math.floor(Math.random() * choice.length)];
 };
+// added the parse into the equation today did not work how i wanted it to
+ 
+// let answer = (`Rock, Paper, Scissors`){
+//  if (answer = "Rock") {
+//    return ('Rock')
+// } else if (answer = "Paper") {
+//   return ('Paper')
+// } else {
+//   return ('Scissors')
+// }
+// }
+// const choices = parseInt(prompt(`Choose one; ${choice}`))
 
 const computerSelection = computerPlay();
-let playerSelection = "Rock";
+const playerSelection = ('Rock')
+
+// trying to find out how to make the choices input = the playerSelection 
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return "It's a draw";
-  } else if (playerSelection == "Rock" && computerSelection == "Paper") {
-    return "You've Lost!!";
-  } else if (playerSelection == "Paper" && computerSelection == "Rock") {
-    return "Hooray You've Won";
-  } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
-    return "Hooray You've Won";
-  } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
-    return "You've Lost!!";
-  }else if (playerSelection == "Rock" && computerSelection == "Scissors") {
-    return "Hooray You've Won";
-  }else if (playerSelection == "Scissors" && computerSelection == "Rock") {
-    return "You've Lost!!";
-  }
+  return "It's a draw";
+} else if (playerSelection == "Rock" && computerSelection == "Paper") {
+  return "You've Lost!!";
+} else if (playerSelection == "Paper" && computerSelection == "Rock") {
+  return "Hooray You've Won";
+} else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+  return "Hooray You've Won";
+} else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+  return "You've Lost!!";
+}else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+  return "Hooray You've Won";
+}else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+  return "You've Lost!!";
 }
-function game(){
-  
 }
+// tried to figure out how to log the playRound function multiple times to 
+// create the game() function  not sure if i should multiply by 5 or repeat 5x
+console.log(playRound(playerSelection, computerSelection));
